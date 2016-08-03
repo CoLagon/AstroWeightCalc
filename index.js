7b21e3dd4e5c8cb7527c0eccba05f5c46682098b
CoLagon <colelogan19@yahoo.com>
2016-08-02 17:32:05 -0700

    This measures your weight on different planets and stars

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+function calc() {
+	var userWeight = document.getElementById("wt").value;
+	var userPlanet = document.getElementById("pt").value;
+	var result = document.getElementById("Math");
+	result.innerHTML = userWeight * userPlanet;
+
+}
+
